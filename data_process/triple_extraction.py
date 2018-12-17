a4a813aabef2488aefdb4f789aba625945bd293a
BigBigRadish <luozhukun@163.com>
2018-12-17 20:45:33 +0800

    金融文本事件抽取

--- data_process/triple_extraction.py
+++ data_process/triple_extraction.py
@@ -217,16 +217,16 @@ class TripleExtractor:
             o = ''.join([words[word_index] for word_index in range(role_info['A1'][1], role_info['A1'][2]+1) if
                          postags[word_index][0] not in ['w', 'u', 'x'] and words[word_index]])
             if s  and o:
-                return '1', [s, v, o]
+                return '1', str(s+v+o)#[s, v, o]
 #         elif 'A0' in role_info:
 #             s = ''.join([words[word_index] for word_index in range(role_info['A0'][1], role_info['A0'][2] + 1) if
 #                          postags[word_index][0] not in ['w', 'u', 'x']])
 #             if s:
-#                 return '2', [s, v]
+#                 return '2', [s,v]
         elif 'A1' in role_info:
             o = ''.join([words[word_index] for word_index in range(role_info['A1'][1], role_info['A1'][2]+1) if
                          postags[word_index][0] not in ['w', 'u', 'x']])
-            return '3', [v, o]
+            return '3', str(v+o)#[v,o]
         return '4', []
 
     '''三元组抽取主函数'''
@@ -258,7 +258,7 @@ class TripleExtractor:
                         r = words[index]
                         e1 = self.complete_e(words, postags, child_dict_list, child_dict['SBV'][0])
                         e2 = self.complete_e(words, postags, child_dict_list, child_dict['VOB'][0])
-                        svos.append([e1, r, e2])
+                        svos.append(e1+r+e2)#[e1, r, e2]
 
                     # 定语后置，动宾关系
                     relation = arcs[index][0]
@@ -272,19 +272,15 @@ class TripleExtractor:
                             if temp_string == e1[:len(temp_string)]:
                                 e1 = e1[len(temp_string):]
                             if temp_string not in e1:
-                                svos.append([e1, r, e2])
+                                svos.append(e1+r+e2)#[e1, r, e2]
                     # 含有介宾关系的主谓动补关系
-                    elif 'SBV' in child_dict and 'CMP' in child_dict:
+                    if 'SBV' in child_dict and 'CMP' in child_dict:
                         e1 = self.complete_e(words, postags, child_dict_list, child_dict['SBV'][0])
                         cmp_index = child_dict['CMP'][0]
                         r = words[index] + words[cmp_index]
                         if 'POB' in child_dict_list[cmp_index]:
                             e2 = self.complete_e(words, postags, child_dict_list, child_dict_list[cmp_index]['POB'][0])
-                            svos.append([e1, r, e2])
-                    elif postags[index] in ['n','nh','ni','nl','ns','nz','r','ws','j','c','v','i','p']:
-                        words1.append(words[index])
-            if words1!=None:
-                svos.append(words1)                                  
+                            svos.append(e1+r+e2)#[e1, r, e2]                      
         return svos
 
     '''对找出的主语或者宾语进行扩展'''
@@ -346,7 +342,7 @@ if __name__ == '__main__':
     extractor1 = TripleExtractor()
     mongo_con=MongoClient('172.20.66.56', 27017)
     db=mongo_con.Causal_event
-    collection=db.triple_extraction
+    collection=db.triple_extraction_1
     #extractor = CausalitySentencesExractor()
     path = r'E:\\Causal_events\\sina_articles_causality_extract'
     #sentence="我爱你,中国"
@@ -368,22 +364,26 @@ if __name__ == '__main__':
         for index,i in article_causality_sentence.iterrows():
             svos=1
             yuanyin_svos = extractor1.triples_main(i['原因'])#原因三元组对
-            print(yuanyin_svos)
+        
             tag=i['标签']#事件标签
             jieguo_svos = extractor1.triples_main(i['结果'])#结果三元组对
+            
             if len(yuanyin_svos)==0:
                 yuanyin_svos.append(i['原因'])
                 svos=0
             if len(jieguo_svos)==0:
                 jieguo_svos.append(i['结果'])
                 svos=0    
-#             yuanyin.append(yuanyin_svos)
-#             jieguo.append(jieguo_svos)             
-#             collection.insert({'栏目':'50人经济论坛','文件名':i['文件名'],'原因三元组':yuanyin_svos,'结果三元组':jieguo_svos,'标签':tag,'svos':svos})
-#         article_causality_sentence['原因三元组']=yuanyin
-#         article_causality_sentence['结果三元组']=jieguo
+            yuanyin.append(yuanyin_svos)
+            jieguo.append(jieguo_svos) 
+            print('原因:'+str(yuanyin_svos)) 
+            print('结果:'+str(jieguo_svos))           
+#             collection.insert({'栏目':'sina财经评论','文件名':i['文件名'],'原因三元组':yuanyin_svos,'结果三元组':jieguo_svos,'标签':tag,'svos':svos})
+#         article_causality_sentence['原因三元组']=str(yuanyin)
+#         article_causality_sentence['结果三元组']=str(jieguo)
 #         article_causality_sentence.to_csv('E:\\Causal_events\\sina_economics_triple_extraction\\'+str(file))
-# #               
+#         article_causality_sentence.to_csv('E:\\Causal_events\\sina_economics_triple_extraction\\sina_economics_triple_extraction.csv','a')
+#               
             
     
     
