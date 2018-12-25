ea99547657d559416227030800299b9cf631b9c6
BigBigRadish <luozhukun@163.com>
2018-12-25 20:34:21 +0800

    三元组抽取

--- data_process/triple_extraction.py
+++ data_process/triple_extraction.py
@@ -14,191 +14,6 @@ import os
 from pymongo import MongoClient
 import codecs
 import pandas as pd
-# class CausalitySentencesExractor():#提取因果句子
-#     def __init__(self):
-#         pass
-# 
-#     '''1由果溯因配套式'''
-#     def ruler1(self, sentence):
-#         '''
-#         conm2:〈[之]所以,因为〉、〈[之]所以,由于〉、 <[之]所以,缘于〉
-#         conm2_model:<Conj>{Effect},<Conj>{Cause}
-#         '''
-#         datas = []
-#         word_pairs =[['之?所以', '因为'], ['之?所以', '由于'], ['之?所以', '缘于']]
-#         for word in word_pairs:
-#             pattern = re.compile(r'\s?(%s)/[p|c]+\s(.*)(%s)/[p|c]+\s(.*)' % (word[0], word[1]))
-#             result = pattern.findall(sentence)
-#             if result:
-#                 return sentence
-#     '''2由因到果配套式'''
-#     def ruler2(self, sentence):
-#         '''
-#         conm1:〈因为,从而〉、〈因为,为此〉、〈既[然],所以〉、〈因为,为此〉、〈由于,为此〉、〈只有|除非,才〉、〈由于,以至[于]>、〈既[然],却>、
-#         〈如果,那么|则〉、<由于,从而〉、<既[然],就〉、〈既[然],因此〉、〈如果,就〉、〈只要,就〉〈因为,所以〉、 <由于,于是〉、〈因为,因此〉、
-#          <由于,故〉、 〈因为,以致[于]〉、〈因为,因而〉、〈由于,因此〉、<因为,于是〉、〈由于,致使〉、〈因为,致使〉、〈由于,以致[于] >
-#          〈因为,故〉、〈因[为],以至[于]>,〈由于,所以〉、〈因为,故而〉、〈由于,因而〉
-#         conm1_model:<Conj>{Cause}, <Conj>{Effect}
-#         '''
-#         datas = []
-#         word_pairs =[['因为', '从而'], ['因为', '为此'], ['既然?', '所以'],
-#                     ['因为', '为此'], ['由于', '为此'], ['除非', '才'],
-#                     ['只有', '才'], ['由于', '以至于?'], ['既然?', '却'],
-#                     ['如果', '那么'], ['如果', '则'], ['由于', '从而'],
-#                     ['既然?', '就'], ['既然?', '因此'], ['如果', '就'],
-#                     ['只要', '就'], ['因为', '所以'], ['由于', '于是'],
-#                     ['因为', '因此'], ['由于', '故'], ['因为', '以致于?'],
-#                     ['因为', '以致'], ['因为', '因而'], ['由于', '因此'],
-#                     ['因为', '于是'], ['由于', '致使'], ['因为', '致使'],
-#                     ['由于', '以致于?'], ['因为', '故'], ['因为?', '以至于?'],
-#                     ['由于', '所以'], ['因为', '故而'], ['由于', '因而']]
-# 
-#         for word in word_pairs:
-#             pattern = re.compile(r'\s?(%s)/[p|c]+\s(.*)(%s)/[p|c]+\s(.*)' % (word[0], word[1]))
-#             result = pattern.findall(sentence)
-#             if result:
-#                 return sentence
-#             
-#     '''3由因到果居中式明确'''
-#     def ruler3(self, sentence):
-#         '''
-#         cons2:于是、所以、故、致使、以致[于]、因此、以至[于]、从而、因而
-#         cons2_model:{Cause},<Conj...>{Effect}
-#         '''
-#         datas=[]
-#         pattern = re.compile(r'(.*)[,，]+.*(于是|所以|故|致使|以致于?|因此|以至于?|从而|因而)/[p|c]+\s(.*)')
-#         result = pattern.findall(sentence)
-#         str1=''
-#         data = dict()
-#         if result:
-#             return sentence
-#     '''4由因到果居中式精确'''
-#     def ruler4(self, sentence):
-#         '''
-#         verb1:牵动、导向、使动、导致、勾起、引入、指引、使、予以、产生、促成、造成、引导、造就、促使、酿成、
-#             引发、渗透、促进、引起、诱导、引来、促发、引致、诱发、推进、诱致、推动、招致、影响、致使、滋生、归于、
-#             作用、使得、决定、攸关、令人、引出、浸染、带来、挟带、触发、关系、渗入、诱惑、波及、诱使
-#         verb1_model:{Cause},<Verb|Adverb...>{Effect}
-#         '''
-#         datas=[]
-#         pattern = re.compile(r'(.*)\s+(牵动|已致|导向|使动|导致|勾起|引入|指引|使|予以|产生|促成|造成|引导|造就|促使|酿成|引发|渗透|促进|引起|诱导|引来|促发|引致|诱发|推进|诱致|推动|招致|影响|致使|滋生|归于|作用|使得|决定|攸关|令人|引出|浸染|带来|挟带|触发|关系|渗入|诱惑|波及|诱使)/[d|v]+\s(.*)')
-#         result = pattern.findall(sentence)
-#         if result:
-#             return sentence
-# 
-#     '''5由因到果前端式模糊'''
-#     def ruler5(self, sentence):
-#         '''
-#         prep:为了、依据、为、按照、因[为]、按、依赖、照、比、凭借、由于
-#         prep_model:<Prep...>{Cause},{Effect}
-#         '''
-#         pattern = re.compile(r'\s?(为了|依据|按照|因为|因|按|依赖|凭借|由于)/[p|c]+\s(.*)[,，]+(.*)')
-#         result = pattern.findall(sentence)
-#         data = dict()
-#         if result:
-#             return sentence
-# 
-# 
-#     '''6由因到果居中式模糊'''
-#     def ruler6(self, sentence):
-#         '''
-#         adverb:以免、以便、为此、才
-#         adverb_model:{Cause},<Verb|Adverb...>{Effect}
-#         '''
-#         datas=[]
-#         pattern = re.compile(r'(.*)(以免|以便|为此|才)\s(.*)')
-#         result = pattern.findall(sentence)
-#         data = dict()
-#         if result:
-#             return sentence
-#  
-# 
-#     '''7由因到果前端式精确'''
-#     def ruler7(self, sentence):
-#         '''
-#         cons1:既[然]、因[为]、如果、由于、只要
-#         cons1_model:<Conj...>{Cause},{Effect}
-#         '''
-#         datas=[]
-#         pattern = re.compile(r'\s?(既然?|因|因为|如果|由于|只要)/[p|c]+\s(.*)[,，]+(.*)')
-#         result = pattern.findall(sentence)
-#         data = dict()
-#         if result:
-#             return sentence
-# 
-#     '''8由果溯因居中式模糊'''
-#     def ruler8(self, sentence):
-#         '''
-#         3
-#         verb2:根源于、取决、来源于、出于、取决于、缘于、在于、出自、起源于、来自、发源于、发自、源于、根源于、立足[于]
-#         verb2_model:{Effect}<Prep...>{Cause}
-#         '''
-#         datas=[]
-#         pattern = re.compile(r'(.*)(根源于|取决|来源于|出于|取决于|缘于|在于|出自|起源于|来自|发源于|发自|源于|根源于|立足|立足于)/[p|c]+\s(.*)')
-#         result = pattern.findall(sentence)
-#         data = dict()
-#         if result:
-#             return sentence
-#     '''9由果溯因居端式精确'''
-#     def ruler9(self, sentence):
-#         '''
-#         cons3:因为、由于
-#         cons3_model:{Effect}<Conj...>{Cause}
-#         '''
-#         datas=[]
-#         pattern = re.compile(r'(.*)是?\s(因为|由于)/[p|c]+\s(.*)')
-#         result = pattern.findall(sentence)
-#         data = dict()
-#         if result:
-#             return sentence
-# 
-#     '''抽取主函数'''
-#     def extract_triples(self, sentence):
-#         infos =[]
-#       #  print(sentence)
-#         if self.ruler1(sentence):
-#             infos.append(self.ruler1(sentence))
-#         elif self.ruler2(sentence):
-#             infos.append(self.ruler2(sentence))
-#         elif self.ruler3(sentence):
-#             infos.append(self.ruler3(sentence))
-#         elif self.ruler4(sentence):
-#             infos.append(self.ruler4(sentence))
-#         elif self.ruler5(sentence):
-#             infos.append(self.ruler5(sentence))
-#         elif self.ruler6(sentence):
-#             infos.append(self.ruler6(sentence))
-#         elif self.ruler7(sentence):
-#             infos.append(self.ruler7(sentence))
-#         elif self.ruler8(sentence):
-#             infos.append(self.ruler8(sentence))
-#         elif self.ruler9(sentence):
-#             infos.append(self.ruler9(sentence))
-# 
-#         return infos
-# 
-#     '''抽取主控函数'''
-#     def extract_main(self, content):
-#         datas=['']
-#         subsents1=[]
-#         sentences = self.process_content(content)
-#         for sentence in sentences:
-#             subsents1.append(sentence)
-#             subsents = self.fined_sentence(sentence)
-#             subsents1.extend(subsents)
-#         for sent in subsents1:
-#             print(sent)
-#             result = self.extract_triples(sent)
-#         return datas
-# 
-#     '''文章分句处理'''
-#     def process_content(self, content):
-#         return [sentence for sentence in SentenceSplitter.split(content) if sentence]
-# 
-#     '''切分最小句'''
-#     def fined_sentence(self, sentence):
-#         return re.split(r'[？！，；]', sentence)
-
 class TripleExtractor:
     def __init__(self):
         self.parser = LtpParser()
@@ -244,9 +59,9 @@ class TripleExtractor:
 #                 elif flag=='2':
 #                     svos.append(triple)
 #                     tmp = 0
-                elif flag=='3':
-                    svos.append(triple)
-                    tmp = 0
+#                 elif flag=='3':
+#                     svos.append(triple)
+#                     tmp = 0
             if tmp == 1:
                 # 如果语义角色标记为空，则使用依存句法进行抽取
                 # if postags[index] == 'v':
@@ -338,7 +153,7 @@ class TripleExtractor:
 #     svos = extractor.triples_main(content2)
 #     print('svos', svos)
 # 
-# test()
+# test()  
 if __name__ == '__main__':
     extractor1 = TripleExtractor()
     mongo_con=MongoClient('172.20.66.56', 27017)
@@ -404,12 +219,12 @@ if __name__ == '__main__':
             jieguo.append(jieguo_svos_sj1) 
             print('原因:'+yuanyin_svos_sj1) 
             print('结果:'+jieguo_svos_sj1)
-            if yuanyin_svos_sj1 and jieguo_svos_sj1:
-                        collection.insert({'栏目':'50人论坛财经评论','文件名':i['文件名'],'原因事件':yuanyin_svos_sj1,'结果事件':jieguo_svos_sj1,'标签':tag,'svos':svos})
-        article_causality_sentence['原因事件']=yuanyin
-        article_causality_sentence['结果事件']=jieguo
-        article_causality_sentence['标签']=tag1
-        article_causality_sentence.to_csv('E:\\Causal_events\\forum50_articles_sj_extract\\'+str(file))
+#             if yuanyin_svos_sj1 and jieguo_svos_sj1:
+#                         collection.insert({'栏目':'50人论坛财经评论','文件名':i['文件名'],'原因事件':yuanyin_svos_sj1,'结果事件':jieguo_svos_sj1,'标签':tag,'svos':svos})
+#         article_causality_sentence['原因事件']=yuanyin
+#         article_causality_sentence['结果事件']=jieguo
+#         article_causality_sentence['标签']=tag1
+#         article_causality_sentence.to_csv('E:\\Causal_events\\forum50_articles_sj_extract\\'+str(file))
 #         article_causality_sentence.to_csv('E:\\Causal_events\\sina_economics_sj_extraction.csv',mode='a')
                
             
