a66c246b8a037a3a33c251762d72536a4f2dacf1
BigBigRadish <luozhukun@163.com>
2019-05-23 21:52:59 +0800

    一定要注释

--- data_process/causality_extract.py
+++ data_process/causality_extract.py
@@ -5,7 +5,7 @@ Created on 2018年11月17日
 @author: Zhukun Luo
 Jiangxi university of finance and economics
 '''
-import re, jieba
+import re
 import jieba.posseg as pseg
 from pyltp import SentenceSplitter
 import os
@@ -17,7 +17,7 @@ class CausalityExractor():
         pass
 
     '''1由果溯因配套式'''
-    def ruler1(self, sentence):
+    def ruler1(self, sentence): 
         '''
         conm2:〈[之]所以,因为〉、〈[之]所以,由于〉、 <[之]所以,缘于〉
         conm2_model:<Conj>{Effect},<Conj>{Cause}
@@ -25,7 +25,8 @@ class CausalityExractor():
         datas = list()
         word_pairs =[['之?所以', '因为'], ['之?所以', '由于'], ['之?所以', '缘于']]
         for word in word_pairs:
-            pattern = re.compile(r'\s?(%s)/[p|c]+\s(.*)(%s)/[p|c]+\s(.*)' % (word[0], word[1]))
+            pattern = re.compile(r'\s?(%s)/[p|c]+\s(.*)(%s)/[p|c]+\s(.*)' % (word[0], word[1]))#将句子中的词和词性进行了拼接
+            print(sentence)
             result = pattern.findall(sentence)
             data = dict()
             if result:
@@ -61,7 +62,9 @@ class CausalityExractor():
 
         for word in word_pairs:
             pattern = re.compile(r'\s?(%s)/[p|c]+\s(.*)(%s)/[p|c]+\s(.*)' % (word[0], word[1]))
+            print(pattern)
             result = pattern.findall(sentence)
+            print(result)
             data = dict()
             if result:
                 data['tag'] = result[0][0] + '-' + result[0][2]
@@ -221,7 +224,9 @@ class CausalityExractor():
 #         print(subsents1)
         for sent in subsents1:
             sent1 = ' '.join([word.word + '/' + word.flag for word in pseg.cut(sent)])
+#             print(sent1)
             result = self.extract_triples(sent1)
+#             print(result)
             if result:
                 for data in result:
                     if data['tag'] and data['cause'] and data['effect']:
@@ -239,41 +244,41 @@ class CausalityExractor():
 
 
 '''测试'''
-# def test():
-#      content1 = """
-#      截至2008年9月18日12时，5·12汶川地震共造成69227人死亡，374643人受伤，17923人失踪，是中华人民共和国成立以来破坏力最大的地震，也是唐山大地震后伤亡最严重的一次地震。
-#      """
-#      content2 = '''
-#      2015年1月4日下午3时39分左右，贵州省遵义市习水县二郎乡遵赤高速二郎乡往仁怀市方向路段发生山体滑坡，发生规模约10万立方米,导致多辆车被埋，造成交通双向中断。此事故引起贵州省委、省政府的高度重视，省长陈敏尔作出指示，要求迅速组织开展救援工作，千方百计实施救援，减少人员伤亡和财物损失。遵义市立即启动应急救援预案，市应急办、公安、交通、卫生等救援力量赶赴现场救援。目前，灾害已造成3人遇难1人受伤，一辆轿车被埋。
-#      当地时间2010年1月12日16时53分，加勒比岛国海地发生里氏7.3级大地震。震中距首都太子港仅16公里，这个国家的心脏几成一片废墟，25万人在这场骇人的灾难中丧生。此次地震中的遇难者有联合国驻海地维和部队人员，其中包括8名中国维和人员。虽然国际社会在灾后纷纷向海地提供援助，但由于尸体处理不当导致饮用水源受到污染，灾民喝了受污染的水后引发霍乱，已致至少2500多人死亡。
-#      '''
-#      content3 = '''
-#      American Eagle 四季度符合预期 华尔街对其毛利率不满导致股价大跌
-#      我之所以考试没及格，是因为我没有好好学习。
-#      因为天晴了，所以我今天晒被子。
-#      因为下雪了，所以路上的行人很少。
-#      我没有去上课是因为我病了。
-#      因为早上没吃的缘故，所以今天还没到放学我就饿了.
-#      因为小华身体不舒服，所以她没上课间操。
-#      因为我昨晚没睡好，所以今天感觉很疲倦。
-#      因为李明学习刻苦，所以其成绩一直很优秀。
-#      雨水之所以不能把石块滴穿，是因为它没有专一的目标，也不能持之以恒。
-#      他之所以成绩不好，是因为他平时不努力学习。
-#      你之所以提这个问题，是因为你没有学好关联词的用法。
-#      减了税,因此怨声也少些了。
-#      他的话引得大家都笑了，室内的空气因此轻松了很多。
-#      他努力学习，因此通过了考试。
-#      既然明天要下雨，就不要再出去玩。
-#      既然他还是那么固执，就不要过多的与他辩论。
-#      既然别人的事与你无关，你就不要再去过多的干涉。
-#      既然梦想实现不了，就换一个你自己喜欢的梦想吧。
-#      既然别人需要你，你就去尽力的帮助别人。
-#      既然生命突显不出价值，就去追求自己想要的生活吧。
-#      既然别人不尊重你，就不要尊重别人。 因果复句造句
-#      既然题目难做，就不要用太多的时间去想，问一问他人也许会更好。
-#      既然我们是学生，就要遵守学生的基本规范。
-#      '''
-#      extractor = CausalityExractor()
+def test():
+    content1 = """
+     截至2008年9月18日12时，5·12汶川地震共造成69227人死亡，374643人受伤，17923人失踪，是中华人民共和国成立以来破坏力最大的地震，也是唐山大地震后伤亡最严重的一次地震。
+     """
+    content2 = '''
+     2015年1月4日下午3时39分左右，贵州省遵义市习水县二郎乡遵赤高速二郎乡往仁怀市方向路段发生山体滑坡，发生规模约10万立方米,导致多辆车被埋，造成交通双向中断。此事故引起贵州省委、省政府的高度重视，省长陈敏尔作出指示，要求迅速组织开展救援工作，千方百计实施救援，减少人员伤亡和财物损失。遵义市立即启动应急救援预案，市应急办、公安、交通、卫生等救援力量赶赴现场救援。目前，灾害已造成3人遇难1人受伤，一辆轿车被埋。
+     当地时间2010年1月12日16时53分，加勒比岛国海地发生里氏7.3级大地震。震中距首都太子港仅16公里，这个国家的心脏几成一片废墟，25万人在这场骇人的灾难中丧生。此次地震中的遇难者有联合国驻海地维和部队人员，其中包括8名中国维和人员。虽然国际社会在灾后纷纷向海地提供援助，但由于尸体处理不当导致饮用水源受到污染，灾民喝了受污染的水后引发霍乱，已致至少2500多人死亡。
+     '''
+    content3 = '''
+     American Eagle 四季度符合预期 华尔街对其毛利率不满导致股价大跌
+     我之所以考试没及格，是因为我没有好好学习。
+     因为天晴了，所以我今天晒被子。
+     因为下雪了，所以路上的行人很少。
+     我没有去上课是因为我病了。
+     因为早上没吃的缘故，所以今天还没到放学我就饿了.
+     因为小华身体不舒服，所以她没上课间操。
+     因为我昨晚没睡好，所以今天感觉很疲倦。
+     因为李明学习刻苦，所以其成绩一直很优秀。
+     雨水之所以不能把石块滴穿，是因为它没有专一的目标，也不能持之以恒。
+     他之所以成绩不好，是因为他平时不努力学习。
+     你之所以提这个问题，是因为你没有学好关联词的用法。
+     减了税,因此怨声也少些了。
+     他的话引得大家都笑了，室内的空气因此轻松了很多。
+     他努力学习，因此通过了考试。
+     既然明天要下雨，就不要再出去玩。
+     既然他还是那么固执，就不要过多的与他辩论。
+     既然别人的事与你无关，你就不要再去过多的干涉。
+     既然梦想实现不了，就换一个你自己喜欢的梦想吧。
+     既然别人需要你，你就去尽力的帮助别人。
+     既然生命突显不出价值，就去追求自己想要的生活吧。
+     既然别人不尊重你，就不要尊重别人。 因果复句造句
+     既然题目难做，就不要用太多的时间去想，问一问他人也许会更好。
+     既然我们是学生，就要遵守学生的基本规范。
+     '''
+    extractor = CausalityExractor()
 #      path = r'E:\\Causal_events\\forum50_articles'
 #      #sentence="我爱你,中国"
 #      files = os.listdir(path)
@@ -288,50 +293,50 @@ class CausalityExractor():
 #              lines = f1.readlines()
 #          for line in lines:
 #              line+=line   
-#          datas = extractor.extract_main(line)
-#          for data in datas:
-#              print('******'*4)
-#              print('cause', ''.join([word.split('/')[0] for word in data['cause'].split(' ') if word.split('/')[0]]))
-#              print('tag', data['tag'])
-#              print('effect', ''.join([word.split('/')[0] for word in data['effect'].split(' ') if word.split('/')[0]]))
-# test()
-if __name__ == '__main__':
-    mongo_con=MongoClient('172.20.66.56', 27017)
-    db=mongo_con.Causal_event
-    collection=db.forum50_articles_causality_extract_1
-    extractor = CausalityExractor()
-    path = r'E:\\Causal_events\\forum50_articles'
-    #sentence="我爱你,中国"
-    files = os.listdir(path)
-    for file in files :
-        pathname = os.path.join(path, file)
-        print(file)
-        #准确获取一个txt的位置，利用字符串的拼接
-        txt_path = pathname.encode('utf-8')
-        #把结果保存了在contents中
-        with codecs.open(txt_path,'r',encoding='utf8',errors='strict') as f1:
-            try:
-                lines = f1.readlines()
-            except UnicodeDecodeError:#含有gb2312编码
-                f1.encoding='gb2312'
-                lines =f1.readlines()
-            line = str([line.strip() for line in lines]).replace('[','').replace(']','').replace('\\u3000', '')
-        datas = extractor.extract_main(line)
-        biglist=[]
-        for data in datas:
-            cause=''.join([word.split('/')[0] for word in data['cause'].split(' ') if word.split('/')[0]])
-            tag=data['tag']
-            effect=''.join([word.split('/')[0] for word in data['effect'].split(' ') if word.split('/')[0]])
-            yuanju=data['原句']
-            if len(cause)>3 and len(effect)>3:
-                collection.insert({'栏目':'forum50财经经济评论','file':file.replace('.txt',''),'原句':yuanju,'cause':cause,'tag':tag,'effect':effect})
-                print(yuanju)
-                print('tag', data['tag'])
-                print('effect', ''.join([word.split('/')[0] for word in data['effect'].split(' ') if word.split('/')[0]]))
-                print('cause:'+cause)
-#                 biglist.append(['forum50财经经济评论',file.replace('.txt',''),cause,tag,effect])
-            else:
-                continue
+    datas = extractor.extract_main(content3)
+    for data in datas:
+        print('******'*4)
+        print('cause', ''.join([word.split('/')[0] for word in data['cause'].split(' ') if word.split('/')[0]]))
+        print('tag', data['tag'])
+        print('effect', ''.join([word.split('/')[0] for word in data['effect'].split(' ') if word.split('/')[0]]))
+test()
+# if __name__ == '__main__':
+#     mongo_con=MongoClient('172.20.69.233', 27017)
+#     db=mongo_con.Causal_event
+#     collection=db.forum50_articles_causality_extract_1
+#     extractor = CausalityExractor()
+#     path = r'E:\\Causal_events\\sina_economics_articles'
+#     #sentence="我爱你,中国"
+#     files = os.listdir(path)
+#     for file in files :
+#         pathname = os.path.join(path, file)
+#         print(file)
+#         #准确获取一个txt的位置，利用字符串的拼接
+#         txt_path = pathname.encode('utf-8')
+#         #把结果保存了在contents中
+#         with codecs.open(txt_path,'r',encoding='utf8',errors='strict') as f1:
+#             try:
+#                 lines = f1.readlines()
+#             except UnicodeDecodeError:#含有gb2312编码
+#                 f1.encoding='gb2312'
+#                 lines =f1.readlines()
+#             line = str([line.strip() for line in lines]).replace('[','').replace(']','').replace('\\u3000', '')
+#         datas = extractor.extract_main(line)
+#         biglist=[]
+#         for data in datas:
+#             cause=''.join([word.split('/')[0] for word in data['cause'].split(' ') if word.split('/')[0]])
+#             tag=data['tag']
+#             effect=''.join([word.split('/')[0] for word in data['effect'].split(' ') if word.split('/')[0]])
+#             yuanju=data['原句']
+#             if len(cause)>3 and len(effect)>3:
+# #                 collection.insert({'栏目':'sina财经经济评论','file':file.replace('.txt',''),'原句':yuanju,'cause':cause,'tag':tag,'effect':effect})
+#                 print(yuanju)
+#                 print('tag', data['tag'])
+#                 print('effect', ''.join([word.split('/')[0] for word in data['effect'].split(' ') if word.split('/')[0]]))
+#                 print('cause:'+cause)
+# #                 biglist.append(['forum50财经经济评论',file.replace('.txt',''),cause,tag,effect])
+#             else:
+#                 continue
 #         list2=[]#去重
 #         for i in biglist:
 #             if i not in list2:
