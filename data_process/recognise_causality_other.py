a2ff43f983b3f0ff0f97b671b293d3a14ceeb05a
BigBigRadish <luozhukun@163.com>
2019-11-25 21:42:31 +0800

    update

--- data_process/recognise_causality_other.py
+++ data_process/recognise_causality_other.py
@@ -32,9 +32,9 @@ class CausalityExractor():
         datas = list()
         word_pairs =[['之?所以', '因为'], ['之?所以', '由于'], ['之?所以', '缘于']\
                      ,['之?所以', '因'],['之?所以', '鉴于'],['之?所以', '由'],\
-                     ['之?所以', '出于'],['之?所以', '是因为']]
+                     ['之?所以', '出于'],['之?所以', '是因为'],['的目的', '是']]
         for word in word_pairs:
-            pattern = re.compile(r'\s?(%s)/[p|c]+\s(.*)(%s)/[p|c]+\s(.*)' % (word[0], word[1]))#将句子中的词和词性进行了拼接
+            pattern = re.compile(r'\s?(%s)/[p|c|n]+\s(.*)(%s)/[p|c|v]+\s(.*)' % (word[0], word[1]))#将句子中的词和词性进行了拼接
 #             print(sentence)
             result1 = pattern.findall(sentence)
             data = dict()
@@ -50,10 +50,10 @@ class CausalityExractor():
         conm2_model:<Conj>{Effect},<Conj>{Cause}
         '''
         datas = list()
-        word_pairs =[['起','作用'], ['是','原因'],['是','目的']]
+        word_pairs =[['起','作用'], ['是','原因'],['是','目的'],['是','结果'],['是','证明']]
         a=[[],0]
         for word in word_pairs:
-            pattern = re.compile(r'(%s)/[v]+\s(.*)(%s)/[n]+\s(.*)' % (word[0],word[1]))#将句子中的词和词性进行了拼接
+            pattern = re.compile(r'(%s)/[v]+\s(.*)(%s)/[n|v]+\s(.*)' % (word[0],word[1]))#将句子中的词和词性进行了拼接
 #             print(sentence)
             
             result = pattern.findall(sentence)
@@ -133,11 +133,11 @@ class CausalityExractor():
     '''3由因到果居中式明确'''
     def ruler3(self, sentence):
         '''
-        cons2:于是、所以、故、致使、以致[于]、因此、以至[于]、从而、因而、是因为
+        cons2:于是、所以、故、致使、以致[于]、因此、以至[于]、从而、因而、是因为、正因如此、那么
         cons2_model:{Cause},<Conj...>{Effect}
         '''
 #         print(sentence)
-        pattern1 = re.compile(r'(.*)[,，]+.*(于是|所以|故|致使|以致于?|因此|以至于?|从而|因而)/[p|c]+\s(.*)')
+        pattern1 = re.compile(r'(.*)[,，]+.*(由此|那么|让|于是|所以|故|致使|以致于?|因此|以至于?|从而|因而|正因?如此|才能|如此一来)/[p|c|v]+\s(.*)')
         result1 = pattern1.findall(sentence)
         data = dict()
         if result1:
@@ -156,7 +156,7 @@ class CausalityExractor():
         for i in pairs:
             casual_verb+=i+'|'
         casual_verb=casual_verb[0:-1]#去掉最后一个斜杠
-        pattern = re.compile(r'.*[,|，](.*)('+casual_verb+')/[d|v]+\s(.*)')
+        pattern = re.compile(r'.*[,|，](.*)('+casual_verb+')/[p|c|d|v]+\s(.*)')
         result = pattern.findall(sentence)
         data = dict() 
         
@@ -167,10 +167,10 @@ class CausalityExractor():
     '''5由因到果前端式模糊'''
     def ruler5(self, sentence):#去掉'按照'
         '''
-        prep:为了、依据、为、按照、因[为]、按、依赖、照、比、凭借、由于
+        prep:为了、依据、为、按照、因[为]、按、依赖、照、比、凭借、由于、一旦
         prep_model:<Prep...>{Cause},{Effect}
         '''
-        pattern = re.compile(r'\s?(为了|依据|因为|因|按|依赖|凭借|由于)/[p|c]+\s(.*)[,，]+(.*)')
+        pattern = re.compile(r'\s?(为了|依据|因为|因|按|依赖|凭借|由于|一旦)/[d|p|c|v]+\s(.*)[,，]+(.*)')
         result = pattern.findall(sentence)
         data = dict()
         if result:
@@ -184,7 +184,7 @@ class CausalityExractor():
         adverb:以免、以便、为此、才
         adverb_model:{Cause},<Verb|Adverb...>{Effect}
         '''
-        pattern = re.compile(r'(.*)(以免|以便|为此|才)\s(.*)')
+        pattern = re.compile(r'(.*)(以免|以便|为此|才|原因于此)\s(.*)')
         result = pattern.findall(sentence)
         data = dict()
         if result:
@@ -198,7 +198,7 @@ class CausalityExractor():
         cons1:既[然]、因[为]、如果、由于、只要
         cons1_model:<Conj...>{Cause},{Effect}
         '''
-        pattern = re.compile(r'\s?(因|因为|如果|由于|只要)/[p|c]+\s(.*)+(.*)')
+        pattern = re.compile(r'\s?(因|因为|如果|由于|只要|随着|但凡)/[p|c|d|v]+\s(.*)+(.*)')
         result = pattern.findall(sentence)
         data = dict()
         if result:
@@ -213,7 +213,7 @@ class CausalityExractor():
         verb2_model:{Effect}<Prep...>{Cause}
         '''
 
-        pattern = re.compile(r'(.*)(根源于|取决|来源于|出于|取决于|缘于|在于|出自|起源于|来自|发源于|发自|源于|根源于|立足|立足于)/[p|c]+\s(.*)')
+        pattern = re.compile(r'(.*)(确实|根源于|取决|来源于|出于|取决于|缘于|在于|出自|起源于|来自|发源于|发自|源于|根源于|立足|立足于|受制于)/[p|c|d|v]+\s(.*)')
         result = pattern.findall(sentence)
         data = dict()
         if result:
@@ -260,6 +260,8 @@ class CausalityExractor():
             infos.append(self.ruler10(sentence)[0])
         elif self.ruler11(sentence)[1]:
             infos.append(self.ruler11(sentence)[0])
+        elif self.ruler12(sentence)[1]:
+            infos.append(self.ruler12(sentence)[0])
         else:
             return[[],0]
         return [infos,1]
@@ -278,7 +280,7 @@ class CausalityExractor():
 #             print(result)
             if result[1]:
                 mu = threading.Lock()
-                with codecs.open('../data/causality_sentences_d2_1.txt','a',encoding='utf-8') as f:
+                with codecs.open('../data/test_sentences_ture.txt','a',encoding='utf-8') as f:
                     if  mu.acquire(True): #确保文件写入成功
                         f.write(sent.replace('\n','')+'  '+str(result[0]).replace('[','').replace(']','')+'\n')
                         f.flush()
@@ -287,7 +289,7 @@ class CausalityExractor():
                         mu.release()
 #                 return (sent,result[0])
             else:
-                with codecs.open('../data/other_sentences_d2_1.txt','a',encoding='utf-8') as f2:
+                with codecs.open('../data/test_sentences_false.txt','a',encoding='utf-8') as f2:
                     f2.write(sent+'\n') 
 #         return datas
 
@@ -301,9 +303,23 @@ class CausalityExractor():
 
 if __name__ == '__main__':
     extractor = CausalityExractor()
-    path_= r'../data/other_sentences.txt'
+#     path_= r'../data/other_sentences.txt'
+#     with codecs.open(path_,'r',encoding='utf-8') as fr:
+#         total_content=fr.read()
+#     
+#     extractor.extract_main(total_content)
+    path_= r'../data/test_2.csv'
     with codecs.open(path_,'r',encoding='utf-8') as fr:
-        total_content=fr.read()
-    
-    extractor.extract_main(total_content)
+        total_content=fr.readlines()
+    # for line in total_content:
+    #     if(',1' in line):
+    #         with codecs.open('../data/test_true.txt','a',encoding='utf-8') as fw:
+    #             fw.write(line)
+    #     else:
+    #         with codecs.open('../data/test_false.txt','a',encoding='utf-8') as fw_1:
+    #             fw_1.write(line)
+            
+            
+     
+    extractor.extract_main(''.join([line.split(',')[0] for line in total_content]))
 
